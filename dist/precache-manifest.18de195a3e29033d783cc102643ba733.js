4e8c49a661480adff44bb081703ce3f19e2f6793
Joseph Hassell <josephh2018@gmail.com>
2019-06-02 23:23:15 -0500

    Fixed macOS and iOS incompatibility.
    All Sound files now mp3 with direct links.

---
+++ dist/precache-manifest.18de195a3e29033d783cc102643ba733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52ca8c4248435606c9c2",
+    "url": "./dist/js/app.js"
+  },
+  {
+    "revision": "980e98bd9d895445b3750a8a14a5bb45",
+    "url": "./dist/js/webcomponents-bundle.js"
+  },
+  {
+    "revision": "e1af059965755cea6f216c810a431f04",
+    "url": "index.html"
+  },
+  {
+    "revision": "157a6b95cfd97e22757eeac2a9a2bcbb",
+    "url": "src/styles/app.css"
+  },
+  {
+    "revision": "7dd08699f2c4ce4e6419f33486f05bca",
+    "url": "src/sounds/grindr.mp3"
+  }
+];
